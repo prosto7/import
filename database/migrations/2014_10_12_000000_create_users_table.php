c10c72a4fad923905857ec1f5dcc60dff57226dc
prosto7 <roman1199@mail.ru>
2021-08-10 23:34:28 +0700

    create DB and migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('files', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('Link');
+            $table->float('Size,mb',8,2);
             $table->timestamps();
         });
     }
