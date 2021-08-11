099459341c5e592f0423dd547952b7ecb1dd072d
prosto7 <roman1199@mail.ru>
2021-08-11 20:31:31 +0700

    styles on the main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,53 @@
     <link href="{{asset('/js/app.css')}}" rel="stylesheet">
 </head>
 
-<body class="antialiased">
+<body class="text-center">
 
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Cover</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Contact</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="">
     <div class="container">
+    <table class="table inner cover">
+  <thead class="lead">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody class="lead">
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
         <form action="{{ route('upload')}}" method="post" class="form-group" enctype="multipart/form-data">
             @csrf
             <div class="form-group">
@@ -21,7 +65,16 @@
                 <button type="submit" class="btn btn-success">Send</button>
             </div>
         </form>
+   
     </div>
+    </main>
+
+<footer class="mastfoot mt-auto">
+  <div class="inner">
+    <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+  </div>
+</footer>
+</div>
 </body>
 
 </html>
